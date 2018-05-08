562e749031206a31e560df9ed2b0e0fb59c24010
Carlos Velasco <33700617+cvelac4@users.noreply.github.com>
2018-05-08 14:13:26 -0700

    Update passwordBreaker.js
    I looked at the example you guys found and this is what I got from it. Let me know if I am doing this correctly?

--- passwordBreaker.js
+++ passwordBreaker.js
@@ -1,7 +1,26 @@
+#include <pthread.h>
+#include <unistd.h>
+#include <studio.h>
+#incluse <fontl.h>
+
+#define BUFSIZE 65536
+#define NUMTHREADS 8
+#define Asciicharnum 128
+
+
 const sha256 = require('js-sha256');
+char buf [BUFSIZE];
+char paswordArray [ASCIICHARNUM];
+int parentArray [NUMTHREADS] [ASCIICHRNUM];
+void *charCounter(void *param);
+    
+    typedef struct  
+{
+ 
+
 
 // a-z
-let availableItems = 28;
+let availableItems = 26;
 // max length 6 chars
 let maxLength = 4;
 // attack range
@@ -13,6 +32,81 @@ console.log(`Attacking : ${testPassword} : ${targetHash}`);
 
 // 36^2, 36^1, 36^0 ect
 let splits = [];
+    
+    }passwordThredStruct;
+    
+    
+void *charCounter(void *param) 
+{
+    passwordThredStruct* passwordStruct = (passwordThredStruct*) param; 
+    int locationInFile = passwordStruct->tid * passwordStruct->attackRange;
+    char currentCharacter = buf[locationInFile]; 
+
+    for (int i=0; i <passwordStruct->attackRange && locationInFile < BUFSIZE; i++){
+        currentCharacter = passwordArray[locationInFile++];  
+        parentArray[passwordStruct->tid][currentCharacter]++;  
+    }
+    pthread_exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    
+    if(argc != 2){ 
+        fprintf(stderr,"Wrong usage. Please use the program like so: ./a.out c://directory/filename \n");
+        return -1; 
+    }
+	
+    int file;
+    file = open(argv[1], O_RDONLY); 
+
+    if(file == -1){ 
+        fprintf(stderr, "Couldn't open the file. Try again. Be sure to use the following terminal command: ./a.out c://directory/filename \n");
+        return -1;
+    }
+	
+    size_t fileLength; 
+    fileLength = read(file, buf, BUFSIZE);.
+	
+    if(fileLength == -1){ 
+	fprintf(stderr, "Reading the file went wrong. Try again. \n");
+    }
+	
+    close(file); 
+    int partition = fileLength / NUMTHREADS; 
+    printf("%i threads with partition size of %i\n", NUMTHREADS, partition);
+
+    pthread_t tid[NUMTHREADS];
+    pthread_attr_t attr; 
+    pthread_attr_init(&attr); 
+    passwordThredStruct passwordThreads[NUMTHREADS]; 
+
+    for (int i=0; i<NUMTHREADS; i++) {
+        passwordThreads[i].tid = i; 
+        passwordThreads[i].childPartitionSize = partition; 
+        pthread_create(&tid[i], &attr, charCounter, &passwordThreads[i]); 
+    }
+
+    for (int i=0; i<NUMTHREADS; i++){ 
+        pthread_join(tid[i], NULL); 
+    }
+
+    int sumChar; 
+	
+    for (int j=0; j< ASCIICHARNUM; j++){ 
+	sumChar = 0;
+        for (int k = 0; k < NUMTHREADS; k++){ 
+            sumChar += parentArray[k][j]; 
+        }
+		printf("%i occurences of ", sumChar);
+
+		if (j<=32 || j==127) printf("0x%02x\n", j); 
+		else printf("%c\n", j);
+    }
+	
+    
+    
+    
 for (let i = 0; i < maxLength; i++) {
     splits.push(Math.pow(availableItems, i));
 }
@@ -38,4 +132,7 @@ for (let i = 0; i < attackRange; i++) {
         console.log(`PASSWORD BROKEN : '${mapped}' Attempt number: ${i}`);
         break;
     }
+    return 0;
 }
+    
+
